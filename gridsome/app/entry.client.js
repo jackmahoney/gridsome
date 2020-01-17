ec6552e1bbbfdb73bbc07e2814d2e474955df256
Jack Mahoney <jackmahoney212@gmail.com>
2020-01-17 23:34:00 +0100

    Update entry.client.js

--- gridsome/app/entry.client.js
+++ gridsome/app/entry.client.js
@@ -38,44 +38,44 @@ if (process.env.NODE_ENV === 'production') {
 
 // TODO: remove this behavior
 // let Vue router handle internal URLs for anchors in innerHTML
-document.addEventListener('click', event => {
-  const $el = event.target.closest('a')
-  const { hostname, port } = document.location
+// document.addEventListener('click', event => {
+//   const $el = event.target.closest('a')
+//   const { hostname, port } = document.location
 
-  if (
-    event.defaultPrevented || // disables this behavior
-    event.which !== 1 || // not a left click
-    event.metaKey ||
-    event.altKey ||
-    event.ctrlKey ||
-    event.shiftKey ||
-    $el === null || // no link clicked
-    $el.__gLink__ || // g-link component
-    $el.hostname !== hostname || // external link
-    $el.port !== port || // external link
-    /\.[^.]+$/.test($el.pathname) || // link to a file
-    /\b_blank\b/i.test($el.target) // opens in new tab
-  ) return
+//   if (
+//     event.defaultPrevented || // disables this behavior
+//     event.which !== 1 || // not a left click
+//     event.metaKey ||
+//     event.altKey ||
+//     event.ctrlKey ||
+//     event.shiftKey ||
+//     $el === null || // no link clicked
+//     $el.__gLink__ || // g-link component
+//     $el.hostname !== hostname || // external link
+//     $el.port !== port || // external link
+//     /\.[^.]+$/.test($el.pathname) || // link to a file
+//     /\b_blank\b/i.test($el.target) // opens in new tab
+//   ) return
 
-  if (
-    config.pathPrefix &&
-    !$el.pathname.startsWith(config.pathPrefix)
-  ) {
-    return // must include pathPrefix in path
-  }
+//   if (
+//     config.pathPrefix &&
+//     !$el.pathname.startsWith(config.pathPrefix)
+//   ) {
+//     return // must include pathPrefix in path
+//   }
 
-  const path = stripPathPrefix($el.pathname)
-  const { route, location } = router.resolve({
-    path: path + ($el.search || '') + ($el.hash || '')
-  })
+//   const path = stripPathPrefix($el.pathname)
+//   const { route, location } = router.resolve({
+//     path: path + ($el.search || '') + ($el.hash || '')
+//   })
 
-  if (route.name === '*') {
-    return
-  }
+//   if (route.name === '*') {
+//     return
+//   }
 
-  router.push(location, () => {})
-  event.preventDefault()
-}, false)
+//   router.push(location, () => {})
+//   event.preventDefault()
+// }, false)
 
 router.onReady(() => {
   app.$mount('#app')
